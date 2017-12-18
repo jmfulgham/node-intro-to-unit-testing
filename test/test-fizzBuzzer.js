44cabfb71cf90364f862813a1929b3262b65e069
Mone't Fulgham <monetfulgham@gmail.com>
2017-12-18 18:39:34 -0500

    Updated Chai test

--- test/test-fizzBuzzer.js
+++ test/test-fizzBuzzer.js
@@ -1,24 +1,39 @@
-const should= require('chai').should();
+
 const fizzBuzzer= require('../fizzBuzzer');
+const expect= require('chai').expect;
 
 // "describe" is used to declare the entity to be tested, and a callback function that sets up the tests
-describe('fizzBuzzer', function() {
-    it('should divide two numbers and have no remainder', function(){
-  
-    const normalCases=[
-        
-        {a:15, b:5, expected:'buzz'},
-        {a:15, b:3, expected:'fizz'},
-        {a: 15, b:15, expected:'fizz-buzz'}
-    ];
 
-        normalCases.forEach(function(input) {
-        const answer = fizzBuzzer(input.a, input.b);
-        answer.should.equal(input.expected);
-      });
-        });   
-    }
+describe('fizzBuzzer', function() {
+    it('should return fizz if divisible by 3', function(){
+		expect(fizzBuzzer(3)).to.equal('fizz');
+	});
+
+	it('should return buzz if / by 5', function(){
+        expect(fizzBuzzer(5)).to.equal('buzz');
+    })
+
+	it('should be return fizz-buzz if / by 15', function(){
+        expect(fizzBuzzer(15)).to.equal('fizz-buzz');
+    });
+
+
+});
     // indicate the behavior to be tested
     // typically the "it" statement will also include a callback that provides the test
    
-);
+
+
+// describe('#fizzbuzz', function() {
+//     it('should returns fizz if its divisible by 5', function() {
+//      expectedValue.forEach(function(item) {
+//        expect(fizzbuzz(item.num)).to.equal(item.result);
+//      });
+//     });
+//     it('should returns fizz if its divisible by 3', function() {
+//       expect(fizbuzz(3)).to.equal('buzz')
+//     });
+//       it('should returns fizz if its divisible by 15', function() {
+//       expect(fizbuzz(3)).to.equal('buzz')
+//     });
+//   });
